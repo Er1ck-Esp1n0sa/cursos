18416c93ff6a7e6f9aab7da15e0f8a4b3bd74a33
Erick Espinosa Juarez <98977414+Er1ck-Esp1n0sa@users.noreply.github.com>
2023-10-20 14:30:44 +0000

    upodate 2

--- machine/under_over_fit.py
+++ machine/under_over_fit.py
@@ -32,6 +32,8 @@ train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
 #----------------------------------under_over_fit.py ( part 3 )----------------------------------
 
 # compare MAE with differing values of max_leaf_nodes
-for max_leaf_nodes in [5, 50, 500, 5000]:
+#for max_leaf_nodes in [5, 50, 500, 5000]:
+#for max_leaf_nodes in [450, 460, 470, 480, 490, 500, 510, 520]:
+for max_leaf_nodes in [450, 452, 454, 456, 458, 460, 462, 464]:
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
